8fd5a7510561bf1bee1289bab906179c417303f7
jack5505 <jahon.080879@gmail.com>
2018-12-17 18:27:44 +0500

    Start Admin

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Jack on 17.12.2018.
+ */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * Created by Loving on 14.12.2018.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Admin");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
