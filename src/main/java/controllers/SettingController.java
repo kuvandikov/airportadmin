ea503a3ea0b5c9911f575c3c30e7203f3f1a5991
jack5505 <jahon.080879@gmail.com>
2018-12-21 17:11:21 +0500

    Start work in settings

--- src/main/java/controllers/SettingController.java
+++ src/main/java/controllers/SettingController.java
@@ -7,8 +7,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import utils.widgets.MyResourceBundle;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 /**
  * Created by Jack on 21.12.2018.
@@ -32,16 +36,82 @@ public class SettingController implements Initializable {
     private JFXCheckBox englishSelect;
     @FXML
     private JFXButton saveit;
+    private String address;
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        String cwd = new File("").getAbsolutePath();
+        Scanner cine = null;
+        try {
+            cine = new Scanner(new File(cwd + "/src/main/resources/multilanguage/Language.txt"));
+            address = cwd + "/src/main/resources/multilanguage/Language.txt";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String temp = "";
+        temp = cine.next();
+        if(temp.equals("UZB")){
+            uzbekSelect.setSelected(true);
+        }
+        else if(temp.equals("RUS")){
+            rusSelect.setSelected(true);
+        }
+        else if(temp.equals("ENG")){
+            englishSelect.setSelected(true);
+        }
         MyResourceBundle myResourceBundle = new MyResourceBundle(resources.getLocale(),"UTF-8");
         changeLang.setText(myResourceBundle.getString("settings.changeLang"));
         uzbekSelect.setText(myResourceBundle.getString("settings.uzbekLang"));
         rusSelect.setText(myResourceBundle.getString("settings.russianLang"));
         englishSelect.setText(myResourceBundle.getString("settings.englishLang"));
         saveit.setText(myResourceBundle.getString("changePass.save"));
+        checkOnlyOne(uzbekSelect,rusSelect,englishSelect);
+        onClick(saveit);
 
+    }
+
+    private void onClick(JFXButton saveit) {
+        saveit.setOnAction(event ->
+        {
+            System.out.println(address);
+            String temp = "";
+            PrintWriter printWriter = null;
+            try {
+                printWriter = new PrintWriter(new File(address));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            if(uzbekSelect.isSelected()){
+                temp ="UZB";
+            }
+            else if(rusSelect.isSelected()){
+                temp = "RUS";
+            }
+            else if(englishSelect.isSelected()){
+                temp = "ENG";
+            }
+            printWriter.write(temp);
+            printWriter.close();
+
+
+
+        });
+    }
+
+    private void checkOnlyOne(JFXCheckBox uzbekSelect, JFXCheckBox rusSelect, JFXCheckBox englishSelect)
+    {
+        uzbekSelect.setOnAction(event -> {
+            rusSelect.setSelected(false);
+            englishSelect.setSelected(false);
+        });
+        rusSelect.setOnAction(event -> {
+            englishSelect.setSelected(false);
+            uzbekSelect.setSelected(false);
+        });
+        englishSelect.setOnAction(event -> {
+            uzbekSelect.setSelected(false);
+            rusSelect.setSelected(false);
+        });
 
     }
 }
