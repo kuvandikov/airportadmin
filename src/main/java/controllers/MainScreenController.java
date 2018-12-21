62cd6423384c09a5694c5812ecf5267390807010
jack5505 <jahon.080879@gmail.com>
2018-12-21 20:17:32 +0500

    Start working with MainScreen tableView

--- src/main/java/controllers/MainScreenController.java
+++ src/main/java/controllers/MainScreenController.java
@@ -2,8 +2,11 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import models.TableData;
+import utils.widgets.MyResourceBundle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,11 +17,59 @@ import java.util.ResourceBundle;
 public class MainScreenController implements Initializable
 {
     @FXML
-    private TableView<TableData> tableShow;
+    private TableView<TableData> tableShowA;
+
+    @FXML
+    private TableView<TableData> tableShowD;
+
+    @FXML
+    private Tab kelish;
+
+    @FXML
+    private Tab ketish;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowAtime;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowAr;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowAm;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowAs;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowDtime;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowDr;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowDm;
+
+    @FXML
+    private TableColumn<TableData,String> tableShowDs;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        MyResourceBundle myResourceBundle = new MyResourceBundle(resources.getLocale(),"UTF-8");
+        kelish.setText(myResourceBundle.getString("mainScreen.kelish"));
+        ketish.setText(myResourceBundle.getString("mainScreen.ketish"));
+        tableShowAtime.setText(myResourceBundle.getString("mainScreen.tableTime"));
+        tableShowAm.setText(myResourceBundle.getString("mainScreen.tableMarshrut"));
+        tableShowAr.setText(myResourceBundle.getString("mainScreen.tableRace"));
+        tableShowAs.setText(myResourceBundle.getString("mainScreen.tableStatus"));
+        tableShowDtime.setText(myResourceBundle.getString("mainScreen.tableTime"));
+        tableShowDm.setText(myResourceBundle.getString("mainScreen.tableMarshrut"));
+        tableShowDr.setText(myResourceBundle.getString("mainScreen.tableRace"));
+        tableShowDs.setText(myResourceBundle.getString("mainScreen.tableStatus"));
+
+
+
 
     }
 }
